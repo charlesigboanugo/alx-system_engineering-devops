5f45770f2ad5b97782559b53827c5022d60be8a7
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 15:06:08 +0100

    check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,7 +8,7 @@ class flask {
 
   exec { 'install_flask':
     command => '/usr/bin/pip3 install Flask==2.1.0',
-    unless  => '/usr/bin/pip3 show Flask | grep -q Version: 2.1.0',
+    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
     require => Package['python3-pip'],
   }
 }
