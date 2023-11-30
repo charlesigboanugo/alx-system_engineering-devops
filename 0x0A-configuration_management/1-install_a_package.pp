0fbb904f7adbcafd57e386a8169626c16a6038bc
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 23:26:36 +0100

    nice

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,25 @@
-# install Flask
+# 1-install_a_package.pp
 
-exec { 'install python packages':
-  ensure  => '2.1.0',
-  command => 'pip3 install flask',
-  path    => ['/usr/bin/'],
-  }
+class { 'python':
+  version => 'system',
+}
+
+class { 'python::pip':
+  ensure  => 'present',
+  before  => Package['flask'],
+  require => Class['python'],
+}
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip',
+  require  => Class['python::pip'],
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.1.1"',
+  require => Package['flask'],
+}
+
