787a1f973f6bc872a01ecb2bf92f11adbc88284d
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 14:48:18 +0100

    edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,12 @@
 # install flask
 
-class { python:
-  version => system,
+class { 'python':
+  version => 'system',
 }
 
 exec { install_flask:
-  command => /usr/bin/pip3 install Flask,
-  path    => [/usr/bin, /usr/sbin],
-  unless  => /usr/bin/pip3 show Flask,
+  command => '/usr/bin/pip3 install Flask',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show Flask',
 }
 
