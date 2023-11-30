bd9a121e0f80e268c4796623e8151c76af62a290
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 22:57:52 +0100

    edit 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,7 @@
 # install Flask
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
-}
+
+exec { 'install python packages':
+     command   => 'pip3 install flask flask_restful apiai',
+     path => ['/usr/bin/'],
+     unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
+  }
