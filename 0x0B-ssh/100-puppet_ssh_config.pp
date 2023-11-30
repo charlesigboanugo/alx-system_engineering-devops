f1cab36bfe797a993649b9ef6045a5edfa1f42fd
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 22:41:53 +0100

    new

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+file { '/home/your_username/.ssh/config':
+  ensure  => present,
+  content => "Host example.com\n\
+\tIdentityFile ~/.ssh/school\n\
+\tPasswordAuthentication no\n",
+  owner   => 'your_username',
+  group   => 'your_username',
+  mode    => '0600',
+}
+
