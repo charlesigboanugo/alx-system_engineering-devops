abf62602227dae7de4b1aa04127865617fd22fa3
charles igboanugo <charlesigboanugo033@gmail.com>
2023-11-30 15:19:55 +0100

    check

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,5 @@
-# Your Puppet manifest (e.g., install_flask.pp)
-
-# Define a class to manage the installation of Flask
-class flask {
-  package { 'python3-pip':
-    ensure => installed,
-  }
-
-  exec { 'install_flask':
-    command => '/usr/bin/pip3 install Flask==2.1.0',
-    unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-    require => Package['python3-pip'],
-  }
+# install puppet-lint
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
-
-# Include the class to install Flask
-include flask
-
