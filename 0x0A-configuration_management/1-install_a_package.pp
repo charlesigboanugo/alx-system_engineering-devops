8927a7c1897e48d9c1f172e69014adb57a8ab8a1
charles igboanugo <charlesigboanugo033@gmail.com>
2023-12-04 17:34:14 +0100

    good

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,6 +4,6 @@ package { 'pip3':
   ensure => installed
 }
 exec { 'install flask':
-  command => '/usr/bin/pip3 install flask',
+  command => '/usr/bin/pip3 install flask==2.1.0',
   require => Package('pip3')
 }
