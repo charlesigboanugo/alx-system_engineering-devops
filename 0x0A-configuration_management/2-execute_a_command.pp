4112fccd75afc5660ed1b523dd1531e9ddca8c4b
charles igboanugo <charlesigboanugo033@gmail.com>
2024-03-11 10:39:49 +0100

    temp changes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# kill a process named killmenow
-exec { 'kill_process':
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
-  path     => ['/usr/bin', '/usr/sbin',]
-}
--- 0x0D-web_stack_debugging_0/README.md
+++
@@ -1,2 +0,0 @@
-# Web stack debugging #0
-* 0-give_me_a_page: get a response from server
